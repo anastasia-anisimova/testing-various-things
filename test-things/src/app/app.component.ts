12d9aa2a1bd921642a4c1d7185a44da397c7a734
Anastasia <anisimova-nastya2@bk.ru>
2020-06-04 20:36:09 +0300

    feature/test_materialize test materialize/dematerialize

--- test-things/src/app/app.component.ts
+++ test-things/src/app/app.component.ts
@@ -1,20 +1,31 @@
 import {Component, OnInit} from '@angular/core';
 import {TestThingsService} from "./test-things.service";
-import {Observable} from "rxjs";
+import {Observable, Subject} from "rxjs";
+import {debounceTime, flatMap} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'test-things';
   testString$: Observable<string>;
+
+  private refreshSubj = new Subject<void>();
 
   constructor(private testThingsService: TestThingsService) {
   }
 
   ngOnInit() {
-    this.testString$ = this.testThingsService.testHttpRequest();
+    this.testString$ = this.refreshSubj.pipe(
+      debounceTime(250),
+      flatMap(() => this.testThingsService.testHttpRequest()),
+    );
+  }
+
+  setInitData() {
+    //this.testString$ = this.testThingsService.testHttpRequest()
+    this.refreshSubj.next();
   }
 }
--- test-things/src/app/test-things.service.ts
+++ test-things/src/app/test-things.service.ts
@@ -1,16 +1,23 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable, of} from "rxjs";
+import {delay, dematerialize, materialize, tap} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TestThingsService {
   mockHttpClient = {
-    get: of('YES')
+    get: of('YES').pipe(
+      materialize(),
+      delay(3000),
+      dematerialize(),
+      tap(console.log)
+    )
   };
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   testHttpRequest(): Observable<string> {
     return this.mockHttpClient.get;
