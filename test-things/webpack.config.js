50b2662e68fb10ee71781ebf8b52e3b66d7ef7d6
Anisimova Anastasia <anisimova.a7@wildberries.ru>
2020-07-30 13:24:28 +0700

    test webpack config

--- test-things/webpack.config.js
+++ test-things/webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
-  mode:  'development',
+  context: path.resolve(__dirname, 'src/test-webpack'),
+  mode: 'development',
   entry: {
-    main: './src/test-webpack/index.js',
-    analytics: './src/test-webpack/analytics.js'
+    main: './index.js',
+    analytics: './analytics.js'
   },
   output: {
     filename: '[name].[contenthash].js',
@@ -13,7 +15,31 @@ module.exports = {
   },
   plugins: [
     new HTMLWebpackPlugin({
-      template: "./src/test-webpack/index.html"
-    })
-  ]
+      template: "./index.html",
+      minify: {
+        collapseWhitespace: true
+      }
+    }),
+    new CleanWebpackPlugin()
+  ],
+  devServer: {
+    port: 4200
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all"
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.png$/,
+        use: ['file-loader']
+      }
+    ]
+  }
 }
